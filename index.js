c6ef4b58c508ac4fe9a74ace7ef324c72904171a
Patrick A Kim <pkim050@gmail.com>
2018-10-30 02:23:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-function shout(string) {
+function shout(string)
   return string.toUpperCase();
-}
 
-function whisper(string) {
+function whisper(string)
   return string.toLowerCase();
-}
 
-function logShout(string) {
+function logShout(string)
   console.log(string.toUpperCase());
-}
 
-function logWhisper(string) {
+function logWhisper(string)
   console.log(string.toLowerCase());
-}
 
 function sayHiToGrandma(string) {
   var stringTest = "hello!";
