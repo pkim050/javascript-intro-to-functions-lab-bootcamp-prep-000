df874ae30cae33c16ea28bab20f46fd0ae687ae4
Patrick A Kim <pkim050@gmail.com>
2018-10-30 02:15:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string) {
+  return string;
+}
+
+function whisper(string) {
+  return string;
+}
+
+function logShout(string) {
+  console.log(string);
+}
+
+function logWhisper(string) {
+  console.log(string);
+}
+
+function sayHiToGrandma(string) {
+  
+}
